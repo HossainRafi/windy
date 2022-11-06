49ec6e6002de4ccffb3fda6a2ce1edfb2e63704a
HossainRafi <hossainrafi71@gmail.com>
2022-11-06 23:48:21 +0600

    function done for local time and icon URL

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -83,7 +83,14 @@ const getFormattedWeatherData = async (searchParams) => {
   return { ...formattedCurrentWeather, ...formattedForecastWeather };
 };
 
+const formatToLocalTime = (
+  secs,
+  zone,
+  format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
+const iconUrlFromCode = (code) =>
+  `http://openweathermap.org/img/wn/${code}@2x.png`;
 
 export default getFormattedWeatherData;
 
